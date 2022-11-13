9c3088283ee2c21857743fbc1ad799cf2bced761
Agus Romero <agromero@uoc.edu>
2022-11-13 15:25:48 +0100

    Vista Llista Funcionant

--- PlacesToClean/DetailController.swift
+++ PlacesToClean/DetailController.swift
@@ -2,21 +2,54 @@
 //  DetailController.swift
 //  PlacesToClean
 //
-//  Created by AGUS ROMERO on 1/11/22.
+//  Created by AGUS ROMERO on 9/11/22.
 //
 
 import UIKit
 
 class DetailController: UIViewController {
-
+/*
+    @IBOutlet weak var constraintHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var viewPicker: UIPickerView!
+    @IBOutlet weak var imagePicked: UIImageView!
+    @IBOutlet weak var textName: UITextField!
+    @IBOutlet weak var textDescription: UITextView!
+*/
+    var place:Place? = nil
+/*
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      
+        self.constraintHeight.constant = 400
         // Do any additional setup after loading the view.
+        
+        if(place != nil){
+            
+            //Premen a la llista
+            textName.text = place!.name
+            
+            
+            //Temporalment afegim un border a la imatge mentre no disposem d'imatges
+            imagePicked.layer.borderWidth = 1
+            textDescription.text = place!.description
+        }
+        else{
+            
+            // Es nou
+            textName.text = ""
+            textDescription.text = "New Place to be Added"
+       }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
+  
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -29,4 +62,5 @@ class DetailController: UIViewController {
     @IBAction func Close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+ 
 }
--- PlacesToClean/FirstViewController.swift
+++ PlacesToClean/FirstViewController.swift
@@ -7,23 +7,81 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UITableViewController {
+    
+    let m_provider = ManagerPlaces.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let view: UITableView = (self.view as? UITableView)!;
+        view.delegate = self
+        view.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+    //Protocolo Tabla
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        // Número de elmentos del manager
+        
+        return m_provider.GetCount()
+        //return 2
     }
-    */
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Sirve para indicar subsecciones de la lista. En nuestro caso devolvemos
+        // 1 porque no hay subsecciones.
+        return 1;
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Detectar pulsación en un elemento.
+        
+        let place: Place = self.m_provider.GetItemAt(position: indexPath.row)
+        
+        let dc:DetailController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DetailController") as! DetailController
+        dc.place = place
+        present(dc, animated: true, completion: nil)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // Devolver la altura de la fila situada en una posición determinada.
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // devolver una instancia de la clase UITableViewCell que pinte la fila
+        //seleccionada.
+        
+        let celda = UITableViewCell(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: tableView.frame.size.width, height: 100)))
+        
+        celda.textLabel?.text = m_provider.GetItemAt(position: indexPath.row).name
+        
+        //No funciona per pintar el subtítol
+        //celda.detailTextLabel?.text =  m_provider.GetItemAt(position: indexPath.row).description
 
+        return celda
+    }
+    
 }
+
